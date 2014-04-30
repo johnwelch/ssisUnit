92e634998a3b9d80ae6a5264e81afb38d196deac
johnwelch_cp <johnwelch@users.noreply.github.com>
2014-04-30 21:57:58 +0000

    Updates for expanding environment variables in Package Paths, some refactoring to PackageRefs

--- SsisUnit/PackageRef.cs
+++ SsisUnit/PackageRef.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Security;
 using System.Text;
 using System.Xml;
 using System.ComponentModel;
+
+using Microsoft.SqlServer.Dts.Runtime;
+using Microsoft.SqlServer.Management.IntegrationServices;
 
 using SsisUnit.Enums;
 
@@ -13,6 +21,9 @@ namespace SsisUnit
         private string _storageType;
 
         private SecureString _password;
+
+        private Project _project;
+        private Package _package;
 
         public PackageRef(string name, string packagePath, PackageStorageType storageType, string server)
         {
@@ -33,6 +44,131 @@ namespace SsisUnit
         public PackageRef(XmlNode packageRef)
         {
             LoadFromXml(packageRef);
+        }
+
+        public Package LoadPackage()
+        {
+            // TODO: Consider making a singleton instance of this
+            var ssisApp = new Application();
+            Package package = null;
+
+            try
+            {
+                if (StoredPassword != null)
+                {
+#if SQL2005
+                        ssisApp.PackagePassword = Helper.ConvertToUnsecureString(StoredPassword);
+#else
+                    ssisApp.PackagePassword = StoredPassword.ConvertToUnsecureString();
+#endif
+                }
+
+                string password;
+
+                switch (StorageType)
+                {
+                    case PackageStorageType.FileSystem:
+#if SQL2014 || SQL2012
+                        if (string.IsNullOrWhiteSpace(ProjectPath))
+                            package = ssisApp.LoadPackage(ExpandedPackagePath, null);
+                        else
+                        {
+                            password = StoredPassword == null ? null : StoredPassword.ConvertToUnsecureString();
+
+                            // Read the project file into memory and release the file before opening the project.
+                            var fileMemoryStream = new MemoryStream(File.ReadAllBytes(ExpandedProjectPath));
+
+                            _project = string.IsNullOrEmpty(password) ? Project.OpenProject(fileMemoryStream) : Project.OpenProject(fileMemoryStream, password);
+                            _project.ProtectionLevel = DTSProtectionLevel.EncryptSensitiveWithUserKey;
+
+                            package = Helper.LoadPackageFromProject(_project, _project.Name, PackagePath);
+                        }
+#else
+                            package = ssisApp.LoadPackage(ExpandedPackagePath, null);
+#endif
+                        break;
+                    case PackageStorageType.MSDB:
+                        package = ssisApp.LoadFromSqlServer(PackagePath, Server, null, null, null);
+                        break;
+                    case PackageStorageType.PackageStore:
+                        package = ssisApp.LoadFromDtsServer(PackagePath, Server, null);
+                        break;
+                    case PackageStorageType.SsisCatalog:
+#if SQL2014 || SQL2012
+                        password = StoredPassword == null ? null : StoredPassword.ConvertToUnsecureString();
+
+                        var sqlConnectionStringBuilder = new SqlConnectionStringBuilder { DataSource = Server, InitialCatalog = "SSISDB", IntegratedSecurity = true };
+
+                        var integrationServices = new IntegrationServices(new SqlConnection(sqlConnectionStringBuilder.ToString()));
+                        Catalog ssisCatalog = integrationServices.Catalogs.FirstOrDefault();
+
+                        if (ssisCatalog == null)
+                            throw new Exception("A SSIS Catalog could not be found.");
+
+                        string ssisFolderName;
+                        string ssisProjectName;
+
+                        Helper.ParseSsisProjectPath(ProjectPath, out ssisFolderName, out ssisProjectName);
+
+                        CatalogFolder catalogFolder = ssisCatalog.Folders.FirstOrDefault(x => string.Compare(x.Name, ssisFolderName, StringComparison.OrdinalIgnoreCase) == 0);
+
+                        if (catalogFolder == null)
+                            throw new Exception(string.Format("The catalog folder {0} could not be found.", ssisFolderName));
+
+                        ProjectInfo projectInfo = catalogFolder.Projects.FirstOrDefault(x => string.Compare(x.Name, ssisProjectName, StringComparison.OrdinalIgnoreCase) == 0);
+
+                        if (projectInfo == null)
+                            throw new Exception(string.Format("The project {0} could not be found.", ssisProjectName));
+
+                        byte[] projectBytes = projectInfo.GetProjectBytes();
+
+                        if (projectBytes == null || projectBytes.Length < 1)
+                            throw new Exception(string.Format("The project {0} could not be loaded.", ssisProjectName));
+
+                        var catalogMemoryStream = new MemoryStream(projectBytes);
+
+                        _project = password == null ? Project.OpenProject(catalogMemoryStream) : Project.OpenProject(catalogMemoryStream, password);
+                        _project.ProtectionLevel = DTSProtectionLevel.EncryptSensitiveWithUserKey;
+
+                        package = Helper.LoadPackageFromProject(_project, _project.Name, PackagePath);
+
+                        break;
+#else
+                            throw new NotSupportedException();
+#endif
+
+                }
+            }
+            catch (DtsRuntimeException dtsEx)
+            {
+#if SQL2005
+                const string SsisPackageStoreVersion = "2005";
+#elif SQL2008
+                const string SsisPackageStoreVersion = "2008";
+#elif SQL2012
+                const string SsisPackageStoreVersion = "2012";
+#elif SQL2014
+                const string SsisPackageStoreVersion = "2014";
+#endif
+
+                if (StorageType == PackageStorageType.PackageStore && dtsEx.ErrorCode == HResults.DTS_E_PACKAGENOTFOUND)
+                    throw new DtsPackageStoreException(string.Format("The package \"{0}\" couldn't be found in the SSIS {1} Package Store.  Please ensure that the correct unit test engine is used when accessing the SSIS {1} Package Store.", PackagePath, SsisPackageStoreVersion));
+
+                if (dtsEx.ErrorCode == HResults.DTS_E_LOADFROMSQLSERVER)
+                    throw new DtsPackageStoreException(string.Format("There was an error while attempting to load the package \"{0}\" from MSDB.  Please ensure the package path is valid and the correct unit test engine is used to execute the package.  The current unit test engine is SSIS {1}.", PackagePath, SsisPackageStoreVersion));
+
+                if (dtsEx.ErrorCode == HResults.DTS_E_LOADFROMXML)
+                    throw new DtsPackageStoreException(string.Format("There was an error while attempting to load the package \"{0}\" from the file system.  Please ensure the package path is valid and the correct unit test engine is used to execute the package.  The current unit test engine is SSIS {1}.", PackagePath, SsisPackageStoreVersion));
+
+                throw;
+            }
+            catch (KeyNotFoundException)
+            {
+                throw new KeyNotFoundException(string.Format(CultureInfo.CurrentCulture, "The package attribute is {0}, which does not reference a valid package.", PackagePath));
+            }
+
+            _package = package;
+            return package;
         }
 
         [Editor("System.Windows.Forms.Design.FileNameEditor, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a", "System.Drawing.Design.UITypeEditor, System.Drawing, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
@@ -188,5 +324,25 @@ namespace SsisUnit
                 ProjectPath = packageXml.Attributes["projectPath"].Value;
             }
         }
+
+        /// <summary>
+        /// Returns a version of the <see cref="PackagePath"/> with environment variables expanded
+        /// </summary>
+        public string ExpandedPackagePath { get { return Environment.ExpandEnvironmentVariables(PackagePath); } }
+
+        /// <summary>
+        /// Returns a version of the <see cref="ProjectPath"/> with environment variables expanded
+        /// </summary>
+        public string ExpandedProjectPath { get { return Environment.ExpandEnvironmentVariables(ProjectPath); } }
+
+        /// <summary>
+        /// Returns the project associated with the <see cref="PackageRef"/>. <see cref="LoadPackage"/> should be called first for this to be populated.
+        /// </summary>
+        public Project Project { get { return _project; } }
+
+        /// <summary>
+        /// Returns the package associated with the <see cref="PackageRef"/>. <see cref="LoadPackage"/> should be called first for this to be populated.
+        /// </summary>
+        public Package Package { get { return _package; } }
     }
 }
